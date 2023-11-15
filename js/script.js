3d0d90ec42624f483d71cf7f1aebf65738c9530a
Marco <mcruzg1606@alumno.ipn.mx>
2023-11-14 18:27:53 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
-import { API } from 'aws-amplify';
-
 document.getElementById('health-form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevenir el comportamiento de envío predeterminado
-
+    e.preventDefault();  // Prevenir el comportamiento de envío predeterminado
+    
     // Recopilar datos del formulario
     let formData = new FormData(e.target);
     let data = {};
     formData.forEach((value, key) => {
         data[key] = value;
     });
-
-    // Enviar datos al servidor usando AWS Amplify
-    API.post('API', '/prod', { // Asegúrate de que 'API' y '/prod' coincidan con tu configuración de Amplify
-        body: data
+    
+    // Enviar datos al servidor
+    fetch('https://kludndazg8.execute-api.us-east-2.amazonaws.com/prod', { // URL de tu API Gateway
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)  // Convertir datos a formato JSON
     })
     .then(response => {
-        mostrarResultados(response);
+        if (!response.ok) {
+            throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        return response.json();  // Convertir respuesta a formato JSON
+    })
+    .then(data => {
+        // Mostrar los datos en el modal
+        mostrarResultados(data);
     })
     .catch(error => {
-        console.error('Error al procesar la solicitud:', error);
-        alert('Error al procesar la solicitud: ' + error.message); // Mostrar error al usuario
+        console.error('There has been a problem with your fetch operation:', error);
     });
 });
 
 function mostrarResultados(data) {
     let outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    outputDiv.innerHTML = '';
+    if (Array.isArray(data)) {
+        data.forEach(row => {
+            outputDiv.innerHTML += `<pre>${JSON.stringify(row, null, 2)}</pre>`;
+        });
+    } else {
+        outputDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+    }
 
     // Mostrar el modal
     let modal = document.getElementById("myModal");
     modal.style.display = "block";
-}
 
-// Código para manejar el cierre del modal
-let closeButton = document.querySelector(".close");
-closeButton.onclick = function() {
-    let modal = document.getElementById("myModal");
-    modal.style.display = "none";
-}
-
-// Cerrar el modal al hacer clic fuera del contenido
-window.onclick = function(event) {
-    let modal = document.getElementById("myModal");
-    if (event.target == modal) {
+    // Cerrar el modal al hacer clic en el botón de cerrar
+    let closeButton = document.querySelector(".close");
+    closeButton.onclick = function() {
         modal.style.display = "none";
+    }
+
+    // Cerrar el modal al hacer clic fuera del contenido
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
